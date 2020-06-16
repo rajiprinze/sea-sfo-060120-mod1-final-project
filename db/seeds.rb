ae4e84f25ae41924df3d6503f6fa4aeb72908707
keeks5456 <Akikogreen08@gmail.com>
2020-06-16 09:44:35 -0700

    created 14 pokemon so far

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-# House.destroy_all
-# Sorting.destroy_all
-# Student.destroy_all
+# Pokemon.destroy_all
+# Team.destroy_all
+# Trainer.destroy_all
 
 # ravenclaw = House.create(name: "Ravenclaw",
 #   ghost: "The Grey Lady",
@@ -22,9 +22,15 @@
 pikachu = Pokemon.create(name: "Pikachu", level: 52, skill: "Thunder bolt", ability: "Lightening rod")
 bulbasaur = Pokemon.create(name: "Bulbasaur", level: 55, skill: "Solar Bean", ability: "Leaf Guard")
 charmander = Pokemon.create(name: "Charmander", level: 42, skill: "Flame Thrower", ability: "Flash-fire")
-squirttle = Pokemon.create(name: "Squirttle", level: 44, skill: "Waterfall", ability: "Liquid Voice")
-Dargonite = Pokemon.create(name: "Dragonite", level: 52, skill: "Thunder bolt", ability: "Lightening rod")
-pikachu = Pokemon.create(name: "Pikachu", level: 52, skill: "Thunder bolt", ability: "Lightening rod")
-pikachu = Pokemon.create(name: "Pikachu", level: 52, skill: "Thunder bolt", ability: "Lightening rod")
-pikachu = Pokemon.create(name: "Pikachu", level: 52, skill: "Thunder bolt", ability: "Lightening rod")
+squirtle = Pokemon.create(name: "Squirtle", level: 44, skill: "Waterfall", ability: "Liquid Voice")
+dargonite = Pokemon.create(name: "Dragonite", level: 88, skill: "Dragon Breath", ability: "Victory Star")
+gengar = Pokemon.create(name: "Gengar", level: 77, skill: "Night Shade", ability: "Soul-Heart")
+magmar = Pokemon.create(name: "Magmar", level: 66, skill: "Flame Wheel", ability: "Vital Spirit")
+lucario = Pokemon.create(name: "Lucario", level: 78, skill: "Double Kick", ability: "Inner-Focus")
+weavile = Pokemon.create(name: "Weavile", level: 66, skill: "Night Slash", ability: "Pressure")
+milotic = Pokemon.create(name: "Milotic", level: 58, skill: "Aqua Tail", ability: "Marvel Scale")
+absol = Pokemon.create(name: "Absol", level: 88, skill: "Dark Pulse", ability: "Super Luck")
+greninja = Pokemon.create(name: "Greninja", level: 85, skill: "Darkest Lariat", ability: "Torrent")
+mudsdale = Pokemon.create(name: "Mudsdale", level: 79, skill: "High Horsepower", ability: "Own Tempo")
+tyrantrum = Pokemon.create(name: "Tyrantrum", level: 90, skill: "Clanging Scales", ability: "Strong Jaw")
 
