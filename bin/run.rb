8ec9639672d53bee80a894f0947018d067c0f43e
keeks5456 <Akikogreen08@gmail.com>
2020-06-17 17:34:33 -0700

    done with choices, fixed full team, got it to work

--- bin/run.rb
+++ bin/run.rb
@@ -5,21 +5,16 @@ system("clear")
 app = CLI.new
 app.design
 app.welcome_trainer
-puts "-------------------------"
+puts "-------------------------------------------------"
 puts "please press 1, to view your Pokemon database"
-
-# pokemon = gets.chomp
 app.pokemon_database
 puts "Please choose a Pokemon to add to your team"
-# pokemon = gets.chomp
 app.add_pokemon(app.user_input)
 app.choices
-app.view_team
-# binding.pry
+# app.view_team
 puts app.add_pokemon(app.user_input)
-app.view_team
-# binding.pry
-app.too_many_teammates
+# app.view_team
+app.full_team
 app.ending_message 
 
 
